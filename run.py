4a1a0b9e18f13f79de4fa75c390cc5129a192c23
Prokow01 <peterrokowski@gmail.com>
2014-12-14 14:25:15 -0500

    deleted run.yp
    might bring it back if necessary

--- run.py
+++
@@ -1,7 +0,0 @@
-#!interview_site\Scripts\python
-from app import app
-app.run(debug=True)
-
-
-##If you want to make it live and viewable on a local network 
-##replace app.run(debug=true) with app.run('0.0.0.0')
